3e6d6a082b4d5e5283b19fea0e0f5c12df94472a
Amy <AmyLin1928@gmail.com>
2023-11-11 01:32:32 -0500

    setup supertest-jest test

--- server/server.js
+++ server/server.js
@@ -6,4 +6,8 @@ const app = express();
 //sets up middleware
 app.use(express.json());
 
+//sets up custom router middleware
+const authRoute = require("./routes/auth-router");
+app.use("/api", authRoute);
+
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
