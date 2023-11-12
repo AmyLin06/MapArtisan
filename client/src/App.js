4ea35212ecf9a7f06ddcb125e27395ccca9b6ed0
Amy <AmyLin1928@gmail.com>
2023-11-12 18:08:34 -0500

    fixed custom buttom icon prop

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import SplashScreen from "./screens/SplashScreen";
-
+import MapDetailsScreen from "./screens/MapDetailsScreen";
+import currentMap from "./assets/currentMap.json";
 function App() {
   return (
     <div className="App">
+      <header>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@800&display=swap"
+          rel="stylesheet"
+        />
+      </header>
       <SplashScreen />
+      {/* <MapDetailsScreen map={currentMap} /> */}
     </div>
   );
 }
