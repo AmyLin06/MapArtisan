5b096596391d120dacae2855ffa484785d7abc04
lisusan81 <43829072+lisusan81@users.noreply.github.com>
2023-11-12 00:36:12 -0500

    Create confirm modals, and other minor changes to modals (#6)
    * created Confirm Modals
    
    * added documentation to InputModal component
    
    * confirm modal documentation
    
    * undoing accidental change that removed condition on length of input modal

---
+++ client/src/styles/ConfirmModalStyle.js
@@ -0,0 +1,14 @@
+export const ConfirmModalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "45%",
+  transform: "translate(-50%, -50%)",
+  width: "25%",
+  height: "20%",
+  backgroundSize: "contain",
+  backgroundColor: "#FFFDF3",
+  border: "none",
+  boxShadow: "none",
+  color: "#000000",
+  padding: "20px",
+};
