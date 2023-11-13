8500e17ac731a7b818708b4611080c9fcb37d48b
Cha21010 <ronghao.zeng@stonybrook.edu>
2023-11-13 00:23:19 -0500

    ProfileScreenAlmostDone

--- client/src/screens/ProfileScreen.js
+++ client/src/screens/ProfileScreen.js
@@ -5,6 +5,9 @@ import ProfilePicture from "../components/profilepicture.jpg"
 import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Button from '@mui/material/Button';
+import { TextField,Grid,InputLabel,Box} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import CustomButton from "../components/CustomButton";
 
 const ProfileScreen = () => {
     return (
@@ -32,11 +35,175 @@ const ProfileScreen = () => {
                     </span>
                 </Button>
             </div>
-
+            <Typography variant="h5" style={{ textAlign: "left" }}>
+                Name & Username
+            </Typography>
             <div className = "inputs">
-
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <InputLabel htmlFor="firstName" style={{ textAlign: "left" }}>
+                            First Name:
+                         </InputLabel>
+                        <TextField
+                            className="textfield"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="firstName"
+                            label=" Enter First Name"
+                            name="firstName"
+                            autoComplete="first-name"
+                            autoFocus
+                            InputProps={{
+                                style: {
+                                    borderRadius: "50px",
+                                },
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <InputLabel htmlFor="lastName" style={{ textAlign: "left" }}>
+                            Last Name:
+                         </InputLabel>
+                        <TextField
+                            className="textfield"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="lastName"
+                            label=" Enter Last Name"
+                            name="lastName"
+                            autoComplete="family-name"
+                            InputProps={{
+                            style: {
+                                borderRadius: "50px",
+                                color: "navy", // Change the font color to navy blue
+                            },
+                            }}
+                        />
+                    </Grid>
+                    
+                    <Grid item xs={6}>
+                        <InputLabel htmlFor="userName" style={{ textAlign: "left" }}>
+                            UserName:
+                        </InputLabel>
+                        <TextField
+                            className="textfield"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="userName"
+                            label=" Enter Username"
+                            name="userName"
+                            autoComplete="user-name"
+                            InputProps={{
+                            style: {
+                                borderRadius: "50px",
+                                color: "navy", // Change the font color to navy blue
+                            },
+                            }}
+                        />
+                    </Grid>
+                </Grid>
             </div>
 
+            <br></br>
+            <Typography variant="h5" style={{ textAlign: "left" }}>
+                Email & Password
+            </Typography>
+            <div className = "inputs">
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <InputLabel htmlFor="email" style={{ textAlign: "left" }}>
+                            Email:
+                         </InputLabel>
+                        <TextField
+                            className="textfield"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="example@gmail.com"
+                            name="email"
+                            autoComplete=""
+                            autoFocus
+                            InputProps={{
+                                style: {
+                                    borderRadius: "50px",
+                                },
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <InputLabel htmlFor="currentPassword" style={{ textAlign: "left" }}>
+                            Current Password:
+                         </InputLabel>
+                        <TextField
+                            className="textfield"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="currentPassword"
+                            label=" Enter Current Password"
+                            name="currentPassword"
+                            autoComplete=""
+                            InputProps={{
+                            style: {
+                                borderRadius: "50px",
+                                color: "navy", // Change the font color to navy blue
+                            },
+                            }}
+                        />
+                    </Grid>
+                    
+                    <Grid item xs={6}>
+                        <InputLabel htmlFor="newtPassword" style={{ textAlign: "left" }}>
+                            New Password:
+                         </InputLabel>
+                        <TextField
+                            className="textfield"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="newPassword"
+                            label=" Enter New Password"
+                            name="newPassword"
+                            autoComplete=""
+                            InputProps={{
+                            style: {
+                                borderRadius: "50px",
+                                color: "navy", // Change the font color to navy blue
+                            },
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <InputLabel htmlFor="confirmNewPassword" style={{ textAlign: "left" }}>
+                            Confirm New Password:
+                         </InputLabel>
+                        <TextField
+                            className="textfield"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="confirmNewPassword"
+                            label=" Confirm New Password"
+                            name="confirmNewPassword"
+                            autoComplete=""
+                            InputProps={{
+                            style: {
+                                borderRadius: "50px",
+                                color: "navy", // Change the font color to navy blue
+                            },
+                            }}
+                        />
+                    </Grid>
+                </Grid>
+            </div>
+            <br></br>
+            <Box display="flex" justifyContent="flex-end">
+                <CustomButton type="contained" text ="Save Changes"></CustomButton>
+            </Box>
         </div>
       </div>
     );
