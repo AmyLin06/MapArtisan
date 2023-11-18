112d3d931454e6e2d3b98db108d80de7e31c062c
Cha21010 <ronghao.zeng@stonybrook.edu>
2023-11-18 16:47:33 -0500

    test for user

--- server/models/user_model.js
+++ server/models/user_model.js
@@ -7,11 +7,10 @@ const UserSchema = new Schema(
         userName: { type: String, required: true },
         firstName: { type: String, required: true },
         lastName: { type: String, required: true },
-        userName: { type: String, required: true },
         email: { type: String, required: true },
         passwordHash: { type: String, required: true },
         maps: [{type: ObjectId, ref: 'Map'}],
-        profilePicture: { type: ObjectId, ref: 'Image' }
+        // profilePicture: { type: ObjectId, ref: 'Image' }
         //maybe we need a file storage service Amazon S3
         //The String store the URL
     },
