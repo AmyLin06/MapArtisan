a90f599af8c6cda6b4c1f04cc3f0f10f0088c810
SusanLi2024 <susan.li@stonybrook.edu>
2023-11-23 23:21:05 -0500

    added routes for create and delete map on the server side

--- server/routes/map-router.js
+++ server/routes/map-router.js
@@ -1,14 +1,20 @@
-const express = require("express")
-const app = express()
-const UserModel = require('../models/user_model')
+const express = require("express");
+const UserModel = require("../models/user_model");
 const router = express.Router();
+const MapController = require("../controllers/map-controller");
+const auth = require("../auth");
 
-router.route('/get-users').get((req, res) => {
-    UserModel.find({}).then(function(users) {
-        res.json(users)
-    }).catch(function(err) {
-        console.log(err)
+router.route("/get-users").get((req, res) => {
+  UserModel.find({})
+    .then(function (users) {
+      res.json(users);
     })
-})
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+router.post("/createMap", MapController.createMap);
+router.delete("/map/:id", auth.verify, MapController.deleteMap);
 
 module.exports = router;
