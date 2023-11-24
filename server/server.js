ba522c92351bf3c12e2cdd219fd2fb5250ed8876
Amy <AmyLin1928@gmail.com>
2023-11-24 01:00:00 -0500

    remove setting response headers in server

--- server/server.js
+++ server/server.js
@@ -12,44 +12,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 //set up CORS
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000", "https://mapartisan.onrender.com"],
-//     credentials: true,
-//   })
-// );
-
 const corsOptions = require("./config/corsOptions");
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions)); // Handle preflight requests for all routes
-
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    "https://mapartisan.onrender.com",
-    "http://localhost:3000", // Add more origins as needed
-  ];
-
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
 
 app.use(express.json());
 app.use(cookieParser());
